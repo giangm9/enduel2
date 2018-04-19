338b40eb68756db7efd2b644be5ca8e5b89cffe4
giangm9 <giangm9@gmail.com>
2018-04-20 00:59:59 +0700

    add random

--- public/js/dice.js
+++ public/js/dice.js
@@ -1,35 +1,45 @@
 Dice = {
-  fps     : 30.0,
-  nFrame  : 80.0,
-  stop    : 1.5,
-  time    : 0.0,
-  next    : 1.5,
-  high    : false,
-  state   : "idle",
-  urlNorm : "img/norm.jpg",
-  urlHigh : "img/high.jpg",
-  last    : null
+  fps: 30.0,
+  nFrame: 80.0,
+  stop: 1.5,
+  time: 0.0,
+  next: 1.5,
+  high: false,
+  state: "idle",
+  urlNorm: "img/norm.jpg",
+  urlHigh: "img/high.jpg",
+  last: null
 };
 
-Dice.init = function(jqcanvas) {
+Dice.init = function(jqcanvas, click, finish) {
 
-  this.canvas      = jqcanvas[0];
-  this.context     = this.canvas.getContext('2d');
-  this.imgNorm     = new Image();
-  this.imgHigh     = new Image();
+  this.canvas = jqcanvas[0];
+  this.context = this.canvas.getContext('2d');
+  this.imgNorm = new Image();
+  this.imgHigh = new Image();
   this.imgNorm.src = this.urlNorm;
   this.imgHigh.src = this.urlHigh;
+  this.click = click;
+  this.finish = finish;
+
 
   jqcanvas.hover(
-    function() { Dice.high = true; },
-    function() { Dice.high = false;}
+    function() {
+      Dice.high = true;
+    },
+    function() {
+      Dice.high = false;
+    }
   ).click(
-    function() { Dice.state = "spin"; }
+    function() {
+      Dice.state = "spin";
+      Dice.click();
+    }
   );
   Dice.step();
 }
 
-Dice.step = function( timestamp ) {
+Dice.step = function(timestamp) {
   requestAnimationFrame(Dice.step);
 
   if (!Dice.last) Dice.last = timestamp;
@@ -38,6 +48,9 @@ Dice.step = function( timestamp ) {
     Dice.time += (timestamp - Dice.last) * 0.001;
 
     if (Dice.time > Dice.next) {
+      if (Dice.finish) {
+        Dice.finish();
+      }
       Dice.state = "idle";
       Dice.next += Dice.stop;
     }
@@ -51,12 +64,12 @@ Dice.step = function( timestamp ) {
 
 Dice.render = function(index) {
 
-  var width  = 100;
+  var width = 100;
   var height = 100;
-  var nRow   = 8;
-  var nCol   = 10;
-  var row    = Math.floor(index / nCol);
-  var col    = index % nCol;
+  var nRow = 8;
+  var nCol = 10;
+  var row = Math.floor(index / nCol);
+  var col = index % nCol;
   var startX = col * width;
   var startY = row * height;
 
@@ -70,4 +83,3 @@ Dice.render = function(index) {
   )
 
 }
-
